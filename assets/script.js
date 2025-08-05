deb247e795cf42cb1428eab413a79458e6b87645
Dounia <dounia.huber@gmail.com>
2025-08-05 14:17:59 +0200

    feat: ajout des event listeners sur les flèches et un console.log pour tester le fonctionnement

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,15 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+//Je cible les flèches 
+const arrowLeft = document.querySelector(".arrow_left")
+const arrowRight = document.querySelector(".arrow_right")
+
+arrowLeft.addEventListener('click' , () => {
+	console.log('Flèche gauche cliquée')
+})
+
+arrowRight.addEventListener('click' , () => {
+	console.log('Flèche droite cliquée')
+})
