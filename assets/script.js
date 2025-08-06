f3b3b86c17a4f9ce8307f3191f98625afe5b4d89
Dounia <dounia.huber@gmail.com>
2025-08-06 15:26:59 +0200

    feat: ajout de la navigation gauche/droite du carrousel avec mise à jour des bullets, images et textes

--- assets/script.js
+++ assets/script.js
@@ -17,32 +17,56 @@ const slides = [
 	}
 ]
 
-let currentSlide = 0; 			//Déclarer la varible qui Contien la position de la diapositive actuellement affiché(i = 0, je suis à l'image 0)
-const bannerImage = document.querySelector(".banner-img")		//2.Cibler les éléments à modifier (Banner et img)
-const bannerText = document.querySelector("#banner p")							
+let currentSlide = 0; 										//Déclarer la variable qui contient la position de la diapositive actuellement affiché(i = 0, je suis à l'image 0)
+const bannerImg = document.querySelector(".banner-img")		//Cibler l'image dans la bannière
+const bannerText = document.querySelector("#banner p")		//Cibler le texte dans la bannière
+const arrowLeft = document.querySelector(".arrow_left")		//Cibler la flèche gauche
+const arrowRight = document.querySelector(".arrow_right")	//Cibler la flèche droite
 
-//Je cible les flèches 
-const arrowLeft = document.querySelector(".arrow_left")
-const arrowRight = document.querySelector(".arrow_right")
-
-arrowLeft.addEventListener('click' , () => {
-	console.log('Flèche gauche cliquée')
-})
-
-arrowRight.addEventListener('click' , () => {
-	console.log('Flèche droite cliquée')
-})
-		
-//Je rajoute les points sous le carrousel
-const dotsContainer = document.querySelector('.dots')  
+//---Je rajoute les points sous le carrousel---
+const dotsContainer = document.querySelector('.dots')  		//Cibler le conteneur des bullet(dot)s
 
 for (let i = 0; i < slides.length; i++) {
-	const dot = document.createElement('div')	// Crée un petit point
-	dot.classList.add('dot')					// Ajoute la classe CSS "dot"
+	const dot = document.createElement('div')				//Crée un élément <di> pour présenter un point
+	dot.classList.add('dot')								//Ajoute la classe CSS "dot" au point
 
-	if (i === 0) {								
-		dot.classList.add('dot_selected')		// Active le premier point par défaut
+	if (i === 0) {											//Si c'est le premier point (i = 0)
+		dot.classList.add('dot_selected')					//Ajoute la classe "dot_selected" pour l’activer visuellement
 	}
 
-	dotsContainer.appendChild(dot)		 		// Ajoute le/les point(s) au conteneur
+	dotsContainer.appendChild(dot)		 					//Ajoute le/les point(s) au conteneur dans le HTML
 }
+
+const dots = document.querySelectorAll('.dot');				//Récupère tous les éléments ".dot" 
+
+console.log(dots);											//Affiche les dots dans la console (pour vérifier qu'ils existent)
+
+
+//---Flèche gauche---
+arrowLeft.addEventListener('click' , () => {										
+
+	dots[currentSlide].classList.remove("dot_selected")								
+
+	currentSlide--;																	//Je décrémente l'index de la diapo
+
+	dots[currentSlide].classList.add("dot_selected")								
+
+	bannerImg.src = "./assets/images/slideshow/" + slides[currentSlide].image;		
+
+	bannerText.innerHTML = slides[currentSlide].tagLine;							
+})
+
+//---Flèche droite--
+arrowRight.addEventListener('click' , () => {										//Ajouter un écouteur de clic sur la fléche gauche
+
+	dots[currentSlide].classList.remove("dot_selected")								// 1. Je supprime la class "dot_selected" de la dot actuelle 
+
+	currentSlide++; 																// 2. J'incrémente l'index de la diapo
+
+	dots[currentSlide].classList.add("dot_selected")								// 3. J'ajoute la class "dot_selected" de la dot actuelle 
+
+	bannerImg.src = "./assets/images/slideshow/" + slides[currentSlide].image;		// 4. On va chercher le lien de l'image dans le tableau Slides et on modifie l'attribut "src" 
+
+	bannerText.innerHTML = slides[currentSlide].tagLine;							// 5. On va chercher le tagLine de l'image dans le tableau Slides et on l'injecte ds le selecteur "p"
+})
+		
