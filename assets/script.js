e243ee49600a885ee3bf77b14b15798abeaa744e
Dounia <dounia.huber@gmail.com>
2025-08-07 09:40:19 +0200

    chore: harmonisation des guillemets, ajout de points-virgules et commentaires

--- assets/script.js
+++ assets/script.js
@@ -17,35 +17,35 @@ const slides = [
 	}
 ]
 
-let currentSlide = 0; 										//Déclarer la variable qui contient la position de la diapositive actuellement affiché(i = 0, je suis à l'image 0)
-const bannerImg = document.querySelector(".banner-img")		//Cibler l'image dans la bannière
-const bannerText = document.querySelector("#banner p")		//Cibler le texte dans la bannière
-const arrowLeft = document.querySelector(".arrow_left")		//Cibler la flèche gauche
-const arrowRight = document.querySelector(".arrow_right")	//Cibler la flèche droite
+let currentSlide = 0; 											//Déclarer la variable qui contient la position de la diapositive actuellement affiché(i = 0, je suis à l'image 0)
+const bannerImg = document.querySelector(".banner-img");		//Cibler l'image dans la bannière
+const bannerText = document.querySelector("#banner p");			//Cibler le texte dans la bannière
+const arrowLeft = document.querySelector(".arrow_left");		//Cibler la flèche gauche
+const arrowRight = document.querySelector(".arrow_right");		//Cibler la flèche droite
 
 //---Je rajoute les points sous le carrousel---
-const dotsContainer = document.querySelector('.dots')  		//Cibler le conteneur des bullet(dot)s
+const dotsContainer = document.querySelector(".dots");  		//Cibler le conteneur des bullet(dot)s
 
 for (let i = 0; i < slides.length; i++) {
-	const dot = document.createElement('div')				//Crée un élément <di> pour présenter un point
-	dot.classList.add('dot')								//Ajoute la classe CSS "dot" au point
+	const dot = document.createElement("div");				//Crée un élément <di> pour représenter un point
+	dot.classList.add("dot");								//Ajoute la classe CSS "dot" au point
 
 	if (i === 0) {											//Si c'est le premier point (i = 0)
-		dot.classList.add('dot_selected')					//Ajoute la classe "dot_selected" pour l’activer visuellement
+		dot.classList.add("dot_selected");					//Ajoute la classe "dot_selected" pour l’activer visuellement
 	}
 
-	dotsContainer.appendChild(dot)		 					//Ajoute le/les point(s) au conteneur dans le HTML
+	dotsContainer.appendChild(dot);	 						//Ajoute le/les point(s) au conteneur dans le HTML
 }
 
-const dots = document.querySelectorAll('.dot');				//Récupère tous les éléments ".dot" 
+const dots = document.querySelectorAll(".dot");				//Récupère tous les éléments ".dot" 
 
 console.log(dots);											//Affiche les dots dans la console (pour vérifier qu'ils existent)
 
 
 //---Flèche gauche---
-arrowLeft.addEventListener('click' , () => {										
+arrowLeft.addEventListener("click" , () => {										
 
-	dots[currentSlide].classList.remove("dot_selected")								
+	dots[currentSlide].classList.remove("dot_selected");								
 
 	currentSlide--;																	//Je décrémente l'index de la diapo
 
@@ -61,9 +61,9 @@ arrowLeft.addEventListener('click' , () => {
 })
 
 //---Flèche droite--
-arrowRight.addEventListener('click' , () => {										//Ajouter un écouteur de clic sur la fléche gauche
+arrowRight.addEventListener("click" , () => {										//Ajouter un écouteur de clic sur la fléche droite
 
-	dots[currentSlide].classList.remove("dot_selected")								// 1. Je supprime la class "dot_selected" de la dot actuelle 
+	dots[currentSlide].classList.remove("dot_selected");								// 1. Je supprime la class "dot_selected" de la dot actuelle 
 
 	currentSlide++; 																// 2. J'incrémente l'index de la diapo
 
